16285f69d0ff09d32135bcffdf1c0526e4de2ea7
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2023-12-05 17:28:34 -0500

    Restyle dialog box

--- client/src/ModalForm.jsx
+++ client/src/ModalForm.jsx
@@ -6,9 +6,21 @@ export default function ModalForm() {
   const [isOpen, setOpen] = React.useState(false);
   const [url, setUrl] = React.useState("")
 
+  // https://www.w3schools.com/js/js_regexp.asp
+  function verifyPasteUrl(url) {
+    const pattern = /https?:\/\/pokepast\.es\/[0-9a-f]+/
+
+    return pattern.test(url)
+  }
+
   function printLink(event) {
     event.preventDefault()
-    alert(`You're trying to analyze ${url}`)
+
+    if (verifyPasteUrl(url)) {
+      alert(`You're trying to analyze ${url}`)
+    } else {
+      alert('Please provide a Pokepaste URL.')
+    }
     setOpen(false)
     setUrl("")
   }
@@ -20,11 +32,11 @@ export default function ModalForm() {
       * For the idea of making the open state of the modal reactive: https://blog.rixlayer.dev/react-modals
       */}
       <button id="openModal" className="openModal" onClick={() => setOpen(true)}>Add Team</button>
-      <dialog open={isOpen}>
+      <dialog open={isOpen} className="dialog">
 
         {/* https://www.w3schools.com/react/react_forms.asp */}
         <form onSubmit={printLink}>
-          <label>Pokepaste Link:</label>
+          <label><h3 className="linkTitle">Pokepaste Link:</h3></label>
 
           <br />
 
