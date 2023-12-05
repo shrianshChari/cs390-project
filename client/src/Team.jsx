e0e513d4a77481263db38d40b1a5d6e8029d7f2f
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2023-12-05 10:41:13 -0500

    Conditional Rendering

--- client/src/Team.jsx
+++ client/src/Team.jsx
@@ -7,10 +7,25 @@ export default function Team({ team }) {
     <div className="card">
       <SpriteList pokemonList={team.pokemon} />
       <h2>{team.title}</h2>
-      <h3>by {team.author}</h3>
+      <Author author={team.author} />
       {/* rel="noreferrer" kinda came from eslint giving warnings and me being like "Sure, whatever." */}
       <a href={team.pokepaste_url} target="_blank" rel="noreferrer">Link</a>
-      <p>Format: {team.format}</p>
+      <Format format={team.format} />
     </div>
   )
+}
+
+// https://react.dev/learn/conditional-rendering
+function Author({ author }) {
+  if (author.length === 0) {
+    return null;
+  }
+  return <h3>by {author}</h3>
+}
+
+function Format({ format }) {
+  if (format.length === 0) {
+    return null;
+  }
+  return <p>Format: {format}</p>
 }
